1e635416eda5c5fdd738ee9261c6cca711a4d92e
slx_work <3186087995@qq.com>
2019-07-23 17:51:24 +0800

    hah

--- ihome/utils/comments.py
+++ ihome/utils/comments.py
@@ -1,7 +1,23 @@
 from werkzeug.routing import BaseConverter
+import functools
+from flask import g, session, jsonify
+from ihome.utils.response_codes import RET
 
 
 class ReConverter(BaseConverter):
-    def __init__(self,url_map,regex):
+    def __init__(self, url_map, regex):
         super().__init__(url_map)
         self.regex = regex
+
+
+def login_require(view_func):
+    @functools.wraps()
+    def wrapper(*args, **kwargs):
+        user_id = session.get("user_id")
+        if user_id is not None:
+            g.user_id = user_id
+            return view_func(*args, **kwargs)
+        else:
+            return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+
+    return wrapper
